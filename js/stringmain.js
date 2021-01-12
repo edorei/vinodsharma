95d5a80ccf1870eca7525b3bc2cfc1c72711c170
Vinod Sharma <vinod@Vinods-MacBook-Pro.local>
2021-01-12 20:52:10 +0530

    with quote

--- js/stringmain.js
+++ js/stringmain.js
@@ -4,14 +4,20 @@ window.onload=function(){
     console.log("Hello, welcome to vinodsharma.in")
     document.getElementById('convert').addEventListener('click', function() {
         let fromData = document.getElementById("fromData").value;
-        convertToJoinedString(fromData, ",");
+        let instruction = document.querySelector('input[name="flexRadioDefault"]:checked').value;
+        convertToJoinedString(fromData, ",", instruction);
     });
 
 
 
-    function convertToJoinedString(data, token) {
+    function convertToJoinedString(data, token, instruction) {
         let element = document.getElementById("toData");
-        element.value = data.split("\n").join()
+        if(instruction === 'WQ'){
+            element.value = data.split("\n").join()
+        }else if(instruction === 'NWQ'){
+            element.value = "'" + data.split("\n").join('\',\'')
+            element.value = element.value+"'";
+        }
         element.select();
         document.execCommand("copy");
     }  
